33d1d03413c3902b3b46c871d987ea32d474774e
Vixting <vixting@gmail.com>
2025-09-23 12:58:32 +0100

    Testing

--- Content.Shared/_RMC14/TacticalMap/TacticalMapBlip.cs
+++ Content.Shared/_RMC14/TacticalMap/TacticalMapBlip.cs
@@ -6,3 +6,4 @@ namespace Content.Shared._RMC14.TacticalMap;
 [DataRecord]
 [Serializable, NetSerializable]
 public readonly record struct TacticalMapBlip(Vector2i Indices, SpriteSpecifier.Rsi Image, Color Color, TacticalMapBlipStatus Status, SpriteSpecifier.Rsi? Background, bool HiveLeader);
+
